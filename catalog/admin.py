4ed74a4ceef356c27fc368ef5d59f89ba998e87d
misha-cw <bigun.mykhaylo@gmail.com>
2025-08-26 23:49:55 +0300

    feat: Implement admin interfaces for Redactor, Topic, and Newspaper models with custom fieldsets and display options

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Topic, Redactor, Newspaper
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "published_date")
+    list_filter = ("published_date", "topics", "publishers")
+    search_fields = ("title", "content")
+    filter_horizontal = ("topics", "publishers")
