d82f10e201dff4633f7b4b59d76b8046911e79a4
misha-cw <bigun.mykhaylo@gmail.com>
2025-09-15 21:50:58 +0300

    feat: Add TopicListView and corresponding templates for topic management

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     index,
+    TopicListView,
 )
 
 app_name = "catalog"
 
 urlpatterns = [
     path("", index, name="index"),  # Home page
+    path("topics/", TopicListView.as_view(), name="topic-list"),  # Topics list
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Newspaper, Topic, Redactor
 
@@ -16,3 +17,11 @@ def index(request):
     }
 
     return render(request, "catalog/index.html", context=context)
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+    template_name = "catalog/topic_list.html"
+    context_object_name = "topics"
+    paginate_by = 15
+    
